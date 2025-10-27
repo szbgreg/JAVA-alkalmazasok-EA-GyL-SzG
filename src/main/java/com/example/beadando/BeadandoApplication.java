570e6fdd014bd9eb48e2378a3e3120a20d77d8d4
szbgreg <74787170+szbgreg@users.noreply.github.com>
2025-10-27 21:24:42 +0100

    feat: add responsive theme layout with Thymeleaf
    - Added Thymeleaf Layout Dialect dependency
    - Integrated responsive Bootstrap-based theme into static resources
    - Created main layout.html
    - Added index.html extending the layout

--- src/main/java/com/example/beadando/BeadandoApplication.java
+++ src/main/java/com/example/beadando/BeadandoApplication.java
@@ -2,12 +2,19 @@ package com.example.beadando;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class BeadandoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BeadandoApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String showHomePage(){
+        return "index";
+    }
 }
